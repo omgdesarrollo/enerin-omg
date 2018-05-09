fe9e77ff7f61ad0aa25214faf91a031412ef38ce
JoseR <jose_jesus-15@hotmail.com>
2018-05-09 12:30:34 -0500

    JR

--- assets/FileUpload/js/main.js
+++ assets/FileUpload/js/main.js
@@ -18,7 +18,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: '../view/'
+        url: '../View/'
     });
 
     // Enable iframe cross-domain access via redirect option:
