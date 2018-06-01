f18c8a45c86b52a71a6a03fe8daea3228bf40d81
JoseR <jose_jesus-15@hotmail.com>
2018-06-01 09:19:10 -0500

    JR

--- OMG/Controller/OperacionesController.php
+++ OMG/Controller/OperacionesController.php
@@ -47,11 +47,11 @@ switch ($Op)
         echo $res;
         break;
     
-    case 'Modificar':
+    case 'ModificarColumna':
 
-		$data = $model->actualizarPorColumna($_REQUEST["column"],$_REQUEST["editval"],$_REQUEST["id"] );
-		header('Content-type: application/json; charset=utf-8');
-		echo json_encode($data);                  
+		$data = $model->actualizarPorColumna($_REQUEST["COLUMNA"],$_REQUEST["VALOR"],$_REQUEST["ID_EVIDENCIA"] );
+		// header('Content-type: application/json; charset=utf-8');
+		echo $data;
 	break;
     
     
