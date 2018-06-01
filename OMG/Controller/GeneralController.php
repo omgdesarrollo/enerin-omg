b1670a22d2940f7e13ab18a1207bb6f06d6662e9
JoseR <jose_jesus-15@hotmail.com>
2018-06-01 18:57:18 -0500

    JR

--- OMG/Controller/GeneralController.php
+++ OMG/Controller/GeneralController.php
@@ -12,9 +12,13 @@ switch ($Op)
     
     case 'ModificarColumna':
 
-    echo $model->actualizarPorColumna($_REQUEST["TABLA"],$_REQUEST["COLUMNA"],$_REQUEST["VALOR"],$_REQUEST["ID"],$_REQUEST["ID_CONTEXTO"]);
+    $resultado = $model->actualizarPorColumna($_REQUEST["TABLA"],$_REQUEST["COLUMNA"],$_REQUEST["VALOR"],$_REQUEST["ID"],$_REQUEST["ID_CONTEXTO"]);
+    echo $resultado;
     break;
 
+    case 'ListarEvidencia'
+    $resultado = $model->listarEvidencia($_REQUEST['ID_EVIDENCIA']);
+    echo $resulto;
     default: echo false;
     break;
 }
