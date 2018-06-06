987b2b8aab3669afb7734efd6f341f4241f19bb6
fvazconcelos <fvazconcelos@enerin.mx>
2018-06-06 10:17:04 -0500

    fvazconcelos

--- OMG/Controller/DocumentosEntradaController.php
+++ OMG/Controller/DocumentosEntradaController.php
@@ -127,35 +127,8 @@ switch ($Op) {
 		break;
 
 	case 'Modificar':
-		# code...
-   					
-//                  $pojo->setIdDocumentoEntrada($_REQUEST['ID_DOCUMENTO_ENTRADA']);
-//                  $pojo->setIdCumplimiento($_REQUEST['ID_CUMPLIMIENTO']);
-//                  $pojo->setFolioReferencia($_REQUEST['FOLIO_REFERENCIA']);
-//                  $pojo->setFolioEntrada($_REQUEST['FOLIO_ENTRADA']);
-//                  $pojo->set($_REQUEST['FECHA_RECEPCION']);
-//                  $pojo->setAsunto($_REQUEST['ASUNTO']);
-//                  $pojo->setRemitente($_REQUEST['REMITENTE']);
-//                  $pojo->setIdEntidad($_REQUEST['ID_ENTIDAD']);
-//                  $pojo->setIdClausula($_REQUEST['ID_CLAUSULA']);
-//                  $pojo->setClasificacion($_REQUEST['CLASIFICACION']);
-//                  $pojo->setStatusDoc($_REQUEST['STATUS_DOC']);
-//                  $pojo->setFechaAsignacion($_REQUEST['FECHA_ASIGNACION']);
-//                  $pojo->setFechaLimiteAtencion($_REQUEST['FECHA_LIMITE_ATENCION']);
-//                  $pojo->setFechaAlarma($_REQUEST['FECHA_ALARMA']);
-//                  $pojo->setDocumento($_REQUEST['DOCUMENTO']);
-//                  $pojo->setObservaciones($_REQUEST['OBSERVACIONES']);
-//                  
-//                  
-//                  
-//                  
-//                  $model->actualizar($pojo);
-//                  $msg=$exito['mensaje'];
-//                  if($exito['Error']==0){
-//                      header('Content-type: application/json; charset=utf-8');
-//                      echo json_encode(array("data" => $msg));
-//                  }
-		$data = $model->actualizarPorColumna($_REQUEST["column"],$_REQUEST["editval"],$_REQUEST["id"] );
+
+		$data = $model->actualizarPorColumna($_REQUEST["name"],$_REQUEST["value"],$_REQUEST["pk"] );
 		header('Content-type: application/json; charset=utf-8');
 		echo json_encode($data);                  
 	break;
