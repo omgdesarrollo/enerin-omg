968b3afad18139ac11caa86377f8584931e1d2a8
FedericoNC <fnah@enerin.mx>
2018-06-13 19:04:05 -0500

    fn

--- OMG/Controller/AdminController.php
+++ OMG/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 
 session_start();
+require_once '../Model/AdminModel.php';
 require_once '../Model/EmpleadoModel.php';
 // require_once '../Pojo/DocumentoEntradaPojo.php';
 // require_once '../Model/SeguimientoEntradaModel.php';
@@ -11,6 +12,7 @@ require_once '../util/Session.php';
 
 
 $Op=$_REQUEST["Op"];
+$model=new AdminModel();
 $modelEmpleado=new EmpleadoModel();
 // $pojo= new DocumentoEntradaPojo();
 // $modelSeguimientoEntrada=new SeguimientoEntradaModel();
@@ -20,6 +22,15 @@ $modelEmpleado=new EmpleadoModel();
 
 switch ($Op)
 {
+    
+        case 'Listar':
+            $Listar=$model->listarUsuarios();
+            Session::setSesion("listarUsuarios",$Lista);
+            
+            header('Content-type: application/json; charset=utf-8');
+            echo json_encode($Listar);
+            break;
+            
 	case 'BusquedaEmpleado':
 
 		$lista=$modelEmpleado->BusquedaEmpleado($_REQUEST["CADENA"]);
