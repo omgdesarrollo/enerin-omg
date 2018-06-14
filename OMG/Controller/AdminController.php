0c7b36249ab70a6300c94f6b06fae7d3b668a6a0
JoseR <jose_jesus-15@hotmail.com>
2018-06-14 11:02:36 -0500

    JR

--- OMG/Controller/AdminController.php
+++ OMG/Controller/AdminController.php
@@ -11,11 +11,7 @@ require_once '../util/Session.php';
 
 
 $Op=$_REQUEST["Op"];
-<<<<<<< HEAD
 $model = new AdminModel();
-=======
-$model=new AdminModel();
->>>>>>> 968b3afad18139ac11caa86377f8584931e1d2a8
 $modelEmpleado=new EmpleadoModel();
 
 // $pojo= new DocumentoEntradaPojo();
@@ -24,31 +20,23 @@ $modelEmpleado=new EmpleadoModel();
 
 switch ($Op)
 {
-<<<<<<< HEAD
     case 'Listar':
         $lista = $model->listarUsuarios();
         header('Content-type: application/json; charset=utf-8');
         echo json_encode($lista);
     break;
 
-=======
-    
-        case 'Listar':
-            $Listar=$model->listarUsuarios();
-            Session::setSesion("listarUsuarios",$Lista);
-            
-            header('Content-type: application/json; charset=utf-8');
-            echo json_encode($Listar);
-            break;
-            
->>>>>>> 968b3afad18139ac11caa86377f8584931e1d2a8
 	case 'BusquedaEmpleado':
-
 		$lista=$modelEmpleado->BusquedaEmpleado($_REQUEST["CADENA"]);
-    	// Session::setSesion("listarDocumentosEntrada",$Lista);
     	header('Content-type: application/json; charset=utf-8');
 		echo json_encode($lista);
         // return $lista;
+    break;
+
+    case 'ListarPermisos':
+        $lista=$model->listarUsuarioVistas($_REQUEST["ID_USUARIO"]);
+    	header('Content-type: application/json; charset=utf-8');
+		echo json_encode($lista);
     break;
     
     default:
