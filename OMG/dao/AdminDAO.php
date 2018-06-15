0f6357c541e83f6f4c5ed68361aab908a722d2bf
JoseR <jose_jesus-15@hotmail.com>
2018-06-15 13:59:27 -0500

    JR

--- OMG/dao/AdminDAO.php
+++ OMG/dao/AdminDAO.php
@@ -8,13 +8,11 @@ class AdminDAO{
         try
         {
             $query="SELECT tbusuarios.id_usuario, tbusuarios.nombre_usuario,
-		 
-		 tbempleados.nombre_empleado, tbempleados.apellido_paterno, tbempleados.apellido_materno,
-		 tbempleados.categoria, tbempleados.correo  		
-
-                 FROM usuarios tbusuarios
-
-                 JOIN empleados tbempleados ON tbempleados.id_empleado=tbusuarios.id_empleado";
+		    CONCAT (tbempleados.nombre_empleado,' ', tbempleados.apellido_paterno,' ', tbempleados.apellido_materno)
+            AS nombre, tbempleados.categoria, tbempleados.correo
+            FROM usuarios tbusuarios
+            JOIN empleados tbempleados ON tbempleados.id_empleado=tbusuarios.id_empleado
+            WHERE tbusuarios.id_empleado != 0";
             
             $db= AccesoDB::getInstancia();
             $lista = $db->executeQuery($query);
