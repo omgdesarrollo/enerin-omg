90e574605d6b51edf1e74ed30e4ecde62073874c
fvazconcelos <fvazconcelos@enerin.mx>
2018-06-22 12:40:42 -0500

    fvazconcelos

---
+++ OMG/Controller/LoadEstructuraPantallaGeneralController.php
@@ -0,0 +1,26 @@
+<?php
+//este controlador solo atiende un requerimiento
+//el requerimiento que atiende es el de inicio de sesion
+session_start();
+require_once '../Pojo/UsuarioPojo.php';
+require_once '../util/Session.php';
+
+
+
+$Op=$_REQUEST["Op"];
+
+switch ($Op) {
+	case 'Listar':
+           $lista= Session::getSesion("user");
+                header('Content-type: application/json; charset=utf-8');
+                echo json_encode($lista);
+                break;
+ 
+	default:
+		# code...
+		break;
+}
+
+?>
+
+
