58f52c54d38d7a5fd17865f611193f94e3bfaae7
fvazconcelos <fvazconcelos@enerin.mx>
2018-07-05 10:07:11 -0500

    fvazconcelos

--- OMG/Controller/CumplimientosController.php
+++ OMG/Controller/CumplimientosController.php
@@ -46,8 +46,18 @@ switch ($Op) {
 	case 'obtenerContrato':
             
 		$Lista=$model->obtenerContratosPorUsuarioPermiso(Session::getSesion("user")["ID_USUARIO"]);
+                $data;
+                $cont=0;
+                foreach($Lista as $value){
+                    $data[$cont]["list"]=$value["id_cumplimiento"];
+                    $data[$cont]["list"]=$value["clave_cumplimiento"];
+                $cont++;
+                    
+                }
                 header('Content-type: application/json; charset=utf-8');
-                echo json_encode($Lista);
+                
+                echo json_encode($data);
+//                echo json_encode($Lista);
 		return $Lista;
             
 		break;	
--- js/funcionessidebar.js
+++ js/funcionessidebar.js
@@ -10,13 +10,14 @@
                         //alert("en:   ");
 //                     datacontratos.push( {id:'oficio',text:''+,img:'oficios.png',type:'button',isbig:true} )
                      
-                     $.each(r,function(index,value){
-                        // alert("ya entro y "+value.CLAVE_CUMPLIMIENTO);
-                        
-//                      datacontratos.push( {id:'contratos',text:value.clave_cumplimiento,img:'oficios.png',type:'button',isbig:true} );
-//                         datosco   
-                            
-                         })
+//                     $.each(r,function(index,value){
+//                        // alert("ya entro y "+value.CLAVE_CUMPLIMIENTO);
+//                        
+////                      datacontratos.push( {id:'contratos',text:value.clave_cumplimiento,img:'oficios.png',type:'button',isbig:true} );
+////                         datosco   
+//                            
+//                         })
+                         
                          
                          
                         }    
