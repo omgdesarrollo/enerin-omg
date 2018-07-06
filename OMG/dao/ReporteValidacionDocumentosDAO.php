e588b95b34d6488fc84831450aa0115e25b81dff
fvazconcelos <fvazconcelos@enerin.mx>
2018-07-06 12:45:56 -0500

    fvazconcelos

--- OMG/dao/ReporteValidacionDocumentosDAO.php
+++ OMG/dao/ReporteValidacionDocumentosDAO.php
@@ -14,11 +14,12 @@ class ReporteValidacionDocumentosDAO{
 
 		 tbempleados.id_empleado, tbempleados.nombre_empleado, tbempleados.apellido_paterno, tbempleados.apellido_materno,  	
 
-		 tbvalidacion_documento.validacion_tema_responsable	 	
+		 tbvalidacion_documento.validacion_tema_responsable
 
                 FROM validacion_documento tbvalidacion_documento
                 JOIN documentos tbdocumentos ON tbdocumentos.id_documento=tbvalidacion_documento.id_documento
-                JOIN empleados tbempleados ON tbempleados.id_empleado=tbdocumentos.id_empleado";
+                JOIN empleados tbempleados ON tbempleados.id_empleado=tbdocumentos.id_empleado 
+                WHERE tbdocumentos.CONTRATO= ".$v["param"]["contrato"];
             
             $db= AccesoDB::getInstancia();
             $lista = $db->executeQuery($query);
