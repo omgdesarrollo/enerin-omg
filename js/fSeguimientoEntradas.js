f2e8d46eecd4d5d2bf02507801aee6509b34fc75
fvazconcelos <fvazconcelos@enerin.mx>
2018-07-23 16:43:37 -0500

    fvazconcelos

--- js/fSeguimientoEntradas.js
+++ js/fSeguimientoEntradas.js
@@ -19,35 +19,51 @@ function listarDatosSeguimientoEntrada()
 //        nameDate=mostrar_urls(value.id_evidencias);
         __datos.push({
             "No":contador++,
-            "nombre empleado plan":value.nombre_empleadoplan,
-            "nombre empleado tema":value.nombre_empleadotema
+            "folio entrada":value.folio_entrada,
+            "autoridad remitente":value.clave_autoridad,
+            "asunto":value.asunto,
+            "responsable del tema":value.nombre_empleadotema,
+            "fecha asignacion":value.fecha_asignacion,
+            "fecha limite":value.fecha_limite_atencion,
+            "fecha alarma":value.fecha_alarma,
+            "status":value.status_doc,
+            "condicion":"En proceso",
+            "responsable del plan":value.nombre_empleadoplan,
+            "archivo adjunto":value.nombre_empleadoplan,
+            "registrar programa":value.nombre_empleadoplan,
+            "avance del programa":value.avance_programa,
         })
         });
-//        console.log(__datos);
-   }
-   
+  } 
    var listfunciones=[variablefunciondatos];
    ajaxHibrido(datosParamAjaxValues,listfunciones); 
    
        $("#jsGrid").jsGrid({
         width: "100%",
         height: "300px",
+        filtering: true,
         heading: true,
         sorting: true,
         paging: true,
- 
         data: __datos,
         fields: [
                 { name: "No", type: "text", width: 80, validate: "required" },
-                { name: "nombre empleado plan", type: "text", width: 150, validate: "required" },
-                 { name: "nombre empleado tema", type: "text", width: 150, validate: "required" }
-                
+                { name: "folio entrada", type: "text", width: 80, validate: "required" },
+                { name: "autoridad remitente", type: "text", width: 150, validate: "required" },
+                { name: "asunto", type: "text", width: 150, validate: "required" },
+                { name: "responsable del tema", type: "text", width: 150, validate: "required" },
+                { name: "fecha asignacion", type: "text", width: 80, validate: "required" },
+                { name: "fecha limite", type: "text", width: 80, validate: "required" },
+                { name: "fecha alarma", type: "text", width: 80, validate: "required" },
+                { name: "status", type: "text", width: 80, validate: "required" },
+                { name: "condicion", type: "text", width: 80, validate: "required" },
+                { name: "responsable del plan", type: "text", width: 80, validate: "required" },
+                { name: "archivo adjunto", type: "text", width: 80, validate: "required" },
+                { name: "registrar programa", type: "text", width: 80, validate: "required" },
+                { name: "avance del programa", type: "text", width: 80, validate: "required" },
         ]
     });
 }
-
-
-
 
 
 
