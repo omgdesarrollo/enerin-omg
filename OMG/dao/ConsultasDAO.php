e1222f5f928eadf68a4d79bac5e824683b157c57
FedericoNC <fnah@enerin.mx>
2018-08-03 11:50:57 -0500

    fn

---
+++ OMG/dao/ConsultasDAO.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../ds/AccesoDB.php';
+
+class ConsultasDAO{
+
+    public function ListarConsultas()
+    {
+        try
+        {
+            
+        } catch (Exception $ex)
+        {
+            throw $ex;
+            return$ex;
+        }
+    }
+
+
+    
+} 
+
+
+?>
+
