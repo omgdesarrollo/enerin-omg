c55926aab5b842f45a30be1c9259d806bee9cadb
vazconcelos <fvazconcelos@enerin.mx>
2018-08-23 05:11:20 -0500

    vazconcelos

--- OMG/dao/GeneradorReporteDao.php
+++ OMG/dao/GeneradorReporteDao.php
@@ -32,4 +32,35 @@ class GeneradorReporteDao {
             return -1;
         }
     }
+    public function listarReportesDiariosPorFecha(){
+        
+        try {
+            $query="SELECT
+                 tbasignaciones_contrato.region_fiscal,tbasignaciones_contrato.clave_contrato,tbcatalogoproduccion.ubicacion,
+                 tbcatalogoproduccion.tag_patin,tbcatalogoproduccion.tipo_medidor,tbcatalogoproduccion.tag_medidor,tbcatalogoproduccion.clasificacion,
+                 tbcatalogoproduccion.hidrocarburo,  tbomg_reporte_produccion.omgc1,
+		 tbomg_reporte_produccion.omgc2as omgc2,sum(tbomg_reporte_produccion.omgc3)/count(*) as omgc3, sum(tbomg_reporte_produccion.omgc4)/count(*) as omgc4,sum(tbomg_reporte_produccion.omgc5)/count(*) as omgc5,
+		 tbomg_reporte_produccion.omgc6)/count(*) as omgc6, tbomg_reporte_produccion.omgc7, tbomg_reporte_produccion.omgc8, tbomg_reporte_produccion.omgc9,
+		 sum(tbomg_reporte_produccion.omgc10)/count(*) as omgc10,sum(tbomg_reporte_produccion.omgc11)/count(*) as omgc11, sum(tbomg_reporte_produccion.omgc12)/count(*) as omgc12,sum(tbomg_reporte_produccion.omgc13)/count(*) as omgc13,
+		 sum(tbomg_reporte_produccion.omgc14)/count(*) as omgc14,sum(tbomg_reporte_produccion.omgc15)/count(*) as omgc15,sum(tbomg_reporte_produccion.omgc16)/count(*) as omgc16,sum(tbomg_reporte_produccion.omgc17)/count(*) as omgc17,
+		 tbomg_reporte_produccion.omgc18
+                FROM omg_reporte_produccion tbomg_reporte_produccion
+                 JOIN catalogo_produccion tbcatalogoproduccion ON tbcatalogoproduccion.ID_CATALOGOP=tbomg_reporte_produccion.ID_CATALOGOP
+                 JOIN asignaciones_contrato tbasignaciones_contrato ON tbasignaciones_contrato.ID_ASIGNACION=tbcatalogoproduccion.ID_ASIGNACION
+                 WHERE tbomg_reporte_produccion.omgc1 BETWEEN '$FECHA_INICIO' AND '$FECHA_FINAL'  AND
+                 tbasignaciones_contrato.CONTRATO=$CUMPLIMIENTO";
+                     
+                     $db=  AccesoDB::getInstancia();
+                     $lista = $db->executeQuery($query);
+                     return $lista;
+        } catch (Exception $e) {
+            throw $ex;
+            return -1;
+        }
+        
+    }
+    
+    
+    
+    
 }
