cf22177ca04537daa7030146f8e006cc4d69ae5c
FedericoNC <fnah@enerin.mx>
2018-09-14 13:46:42 -0500

    fn

--- OMG/View/TareasView.php
+++ OMG/View/TareasView.php
@@ -83,7 +83,7 @@ $Usuario=  Session::getSesion("user");
             }
             
             .modal-body{color:#888;max-height: calc(100vh - 110px);overflow-y: auto;}                    
-            .modal-lg{width: 50%;}
+            .modal-lg{width: 60%;}
             .modal {/*En caso de que quieras modificar el modal*/z-index: 1050 !important;}
             body{overflow:hidden;}
         </style>              
