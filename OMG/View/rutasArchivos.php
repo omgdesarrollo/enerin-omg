77728fb09c55b48b3edfd70f3964360dd6842d27
JoseR <jose_jesus-15@hotmail.com>
2018-10-05 17:44:04 -0500

    JR

--- OMG/View/rutasArchivos.php
+++ OMG/View/rutasArchivos.php
@@ -1,6 +1,6 @@
 <?php
-    // $urls["fisica"] = "C:xampp/htdocs/enerin-omg/archivos/";
-    // $urls["logica"] = "../../../enerin-omg/archivos/";
+    $urls["fisica"] = "C:xampp/htdocs/enerin-omg/archivos/";
+    $urls["logica"] = "../../../enerin-omg/archivos/";
 
     $urls["fisica"] = "/home/fpa9q09nzhnx/public_html/omgcum/archivos/";
     $urls["logica"] = 'http://www.enerin-omgapps.com/omgcum/archivos/';
