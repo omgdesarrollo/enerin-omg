1118c4162044b1579d1814d0038dc7d1449c58b4
FedericoNC <fnah@enerin.mx>
2018-10-11 16:50:02 -0500

    fn

--- OMG/View/AsignacionTemasRequisitosView.php
+++ OMG/View/AsignacionTemasRequisitosView.php
@@ -142,11 +142,10 @@ $Usuario=  Session::getSesion("user");
                   <option value="ANUAL">ANUAL</option>
                   <option value="BIMESTRAL">BIMESTRAL</option>
                   <option value="DIARIO">DIARIO</option>
+                  <option value="INDEFINIDO">INDEFINIDO</option>
                   <option value="MENSUAL">MENSUAL</option>
                   <option value="POR EVENTO">BIMESTRAL</option>
                   <option value="SEMANAL">SEMANAL</option>                  
-                  <option value="TIEMPO INDEFINIDO">TIEMPO INDEFINIDO</option>
-
               </select>
           </div>
 
