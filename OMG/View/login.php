f77a5888e9eedf6bf5d33dab067a75d4c1f167ce
vazconcelos <desarrollador.frv@gmail.com>
2018-10-18 18:57:31 -0500

    vazconcelos

--- OMG/View/login.php
+++ OMG/View/login.php
@@ -220,6 +220,13 @@ clock();
                     <nav>
                         <div class="nav-wrapper">
                           <div class="col s12">
+                              
+                               <ul class="tabs tabs-fixed-width tab-demo z-depth-1">
+                                    <li class="tab"><a href="#test1">Opcion 1</a></li>
+                                    <li class="tab"><a class="active" href="#test2">Opcion 2</a></li>
+                                    <li class="tab disabled"><a href="#test3">Opcion 3</a></li>
+                                    <li class="tab"><a href="#test4">Opcion 4</a></li>
+                                  </ul>
 <!--                               <div class="carousel">
                                     <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
                                     <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
