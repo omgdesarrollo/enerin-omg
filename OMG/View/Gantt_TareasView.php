0c31eb49a68786e728ce834e8b06838b3ebbbce8
vazconcelos <desarrollador.frv@gmail.com>
2018-11-27 17:46:09 -0600

    vazconcelos

--- OMG/View/Gantt_TareasView.php
+++ OMG/View/Gantt_TareasView.php
@@ -233,9 +233,9 @@ and open the template in the editor.
     position: fixed;
     z-index: 9;
     padding-left: 5%;
-    background-color: #f1f1f1;
+    /*background-color: #f1f1f1;*/
     text-align: center;
-    border: 1px solid #d3d3d3;
+    /*border: 1px solid #d3d3d3;*/
     width: 100%;
 }
 
